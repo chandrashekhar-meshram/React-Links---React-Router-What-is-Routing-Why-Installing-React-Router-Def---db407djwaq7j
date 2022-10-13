4e1732c11a385da2ee6f2559ffc52279c082dd67
CHANDRASHEKHAR MESHRAM <107882415+chandrashekhar-meshram@users.noreply.github.com>
2022-10-13 19:48:01 +0530

    Update ComponentsTopic.js

--- src/components/ComponentsTopic.js
+++ src/components/ComponentsTopic.js
@@ -1,19 +1,25 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const ComponentsTopic = () => {
-  let history = useHistory();
+  //let history = useHistory();
 
-  const backFn = () => {
-    history.goBack();
-  };
+//   const backFn = () => {
+//     history.goBack();
+//   };
+  
+  
 
   return (
     <div id="components-div">
       <h3>Components in React</h3>
-      <button id="components-back-button" onClick={backFn}>
-        Back
-      </button>
+//       <button id="components-back-button" onClick={backFn}>
+//         Back
+//       </button>
+    <button id="components-back-button">
+      <Link to="/topics">Back</Link>
+    </button>
     </div>
   );
 };
